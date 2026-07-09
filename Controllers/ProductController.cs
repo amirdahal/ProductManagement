8091f44eb2873a30ce36e96858db53b0cd93dc8d
amirdahal <amirdahal04@gmail.com>
2026-07-09 13:08:01 +0530

    CRUD implementation on Products

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -65,7 +65,72 @@ public class ProductController : Controller
         _context.Products.Add(newProduct);
         _context.SaveChanges();
 
-        return RedirectToAction("Index");
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("product/update/{id:int}")]
+    public IActionResult Update(int id)
+    {
+        var existingProduct = _context.Products.FirstOrDefault(p => p.Id == id);
+        if (existingProduct == null)
+        {
+            return NotFound($"Product with ID: {id} not found");
+        }
+
+        var updateProductViewModel = new ProductViewModel
+        {
+            Id = existingProduct.Id,
+            Name = existingProduct.Name,
+            SkuCode = existingProduct.SkuCode,
+            Description = existingProduct.Description,
+            UnitPrice = existingProduct.UnitPrice,
+            Category = existingProduct.Category,
+            ImageUrl = existingProduct.ImageUrl
+        };
+
+        return View(updateProductViewModel);
+    }
+
+    [HttpPost("product/update/{id:int}")]
+    public IActionResult Update(int id, ProductViewModel productViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(productViewModel);
+        }
+
+        var product = _context.Products.First(p => p.Id == productViewModel.Id);
+        if (product == null)
+        {
+            return NotFound($"Product with ID: {id} not found");
+        }
+
+        product.Name = productViewModel.Name;
+        product.SkuCode = productViewModel.SkuCode;
+        product.Description = productViewModel.Description;
+        product.UnitPrice = productViewModel.UnitPrice;
+        product.Category = productViewModel.Category;
+        product.ImageUrl = productViewModel.ImageUrl;
+
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Detail), new { id = product.Id });
+    }
+
+    [HttpGet("product/delete/{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound($"Product with ID: {id} not found");
+        }
+
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+
     }
 
 }
--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -4,12 +4,14 @@ namespace ProductManagement.Models;
 
 public class ProductViewModel
 {
-    [Required(ErrorMessage ="Product name is required.")]
-    [StringLength(100, MinimumLength =5, ErrorMessage ="Product name should contain 5 and 100 characters.")]
+    public int Id { get; set; }   // Hidden in the form
+
+    [Required(ErrorMessage = "Product name is required.")]
+    [StringLength(100, MinimumLength = 5, ErrorMessage = "Product name should contain 5 and 100 characters.")]
     public string Name { get; set; } = string.Empty;
 
-    [Required(ErrorMessage ="Product SKU Code is required.")]
-    [StringLength(20, MinimumLength =3, ErrorMessage ="Product SKU Code should contain 3 to 20 characters.")]
+    [Required(ErrorMessage = "Product SKU Code is required.")]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Product SKU Code should contain 3 to 20 characters.")]
     public string SkuCode { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Product description is required.")]
@@ -20,10 +22,10 @@ public class ProductViewModel
     [Range(0.1, 9999, ErrorMessage = "Product price must be greater than 0.")]
     public decimal UnitPrice { get; set; }
 
-    [Required(ErrorMessage ="Product category must be assigned.")]
+    [Required(ErrorMessage = "Product category must be assigned.")]
     public string Category { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Image Url is required.")]
-    [Url(ErrorMessage ="Enter a valid url.")]
+    [Url(ErrorMessage = "Enter a valid url.")]
     public string? ImageUrl { get; set; }
 }
